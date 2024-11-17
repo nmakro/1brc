62e27dab8ee19637f8e087379cc225557a7ad69a
nikos makropoulos <nmakropoulos@gwi.com>
2024-11-17 13:54:15 +0200

    Second revision

--- src/main/go/nmakro/calc/calc.go
+++ src/main/go/nmakro/calc/calc.go
@@ -4,16 +4,22 @@ func CumAverage(index int, prev, current float32) float32 {
 	return (current + (float32(index))*prev) / float32(index+1)
 }
 
-func Min(prev, current float32) float32 {
-	if current < prev {
-		return current
+func Min(index int, prev, current float32) float32 {
+	if index > 0 {
+		if current < prev {
+			return current
+		}
+		return prev
 	}
-	return prev
+	return current
 }
 
-func Max(prev, current float32) float32 {
-	if current > prev {
-		return current
+func Max(index int, prev, current float32) float32 {
+	if index > 0 {
+		if current > prev {
+			return current
+		}
+		return prev
 	}
-	return prev
+	return current
 }
--- src/main/go/nmakro/main.go
+++ src/main/go/nmakro/main.go
@@ -5,12 +5,12 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
 	"runtime"
 	"slices"
-	"sort"
 	"strconv"
 	"sync"
 	"time"
@@ -19,8 +19,6 @@ import (
 	"golang.org/x/exp/maps"
 	"golang.org/x/sync/errgroup"
 )
-
-const linesPerChunk = 100000 // Adjust this value as needed
 
 type Station struct {
 	Name []byte
@@ -36,14 +34,7 @@ type Measurement struct {
 
 type StationMeasurements map[string]Measurement
 
-var Stations = make(StationMeasurements)
-
-func (s StationMeasurements) Sort() {
-	keys := maps.Keys(s)
-	sort.Strings(keys)
-}
-
-func processLines(lines []string) (StationMeasurements, error) {
+func processLines(lines [][]byte) (StationMeasurements, error) {
 	localStations := make(StationMeasurements)
 	for _, line := range lines {
 		splitted := bytes.Split([]byte(line), []byte(";"))
@@ -59,13 +50,26 @@ func processLines(lines []string) (StationMeasurements, error) {
 
 		prevMeasurement := localStations[name]
 		localStations[name] = Measurement{
-			Min:   calc.Min(prevMeasurement.Min, measurement),
-			Max:   calc.Max(prevMeasurement.Max, measurement),
+			Min:   calc.Min(prevMeasurement.Index, prevMeasurement.Min, measurement),
+			Max:   calc.Max(prevMeasurement.Index, prevMeasurement.Max, measurement),
 			Mean:  calc.CumAverage(prevMeasurement.Index, prevMeasurement.Mean, measurement),
 			Index: prevMeasurement.Index + 1,
 		}
 	}
 	return localStations, nil
+}
+
+func skipUntilNewline(reader *bufio.Reader) error {
+	// Skip bytes until we find a newline character
+	for {
+		b, err := reader.ReadByte()
+		if err != nil {
+			return err
+		}
+		if b == '\n' {
+			return nil
+		}
+	}
 }
 
 func main() {
@@ -84,76 +88,112 @@ func main() {
 		log.Fatal(err)
 	}
 
+	numWorkers := runtime.NumCPU() * 20
+
+	fileSize := info.Size()
+
+	linesPerChunk := fileSize / int64(numWorkers)
+
 	fmt.Printf("File size: %d bytes\n", info.Size())
 	defer f.Close()
 
-	numWorkers := runtime.NumCPU()
-
-	results := make(chan StationMeasurements, numWorkers)
-	eg, ctx := errgroup.WithContext(context.Background())
-
-	scanner := bufio.NewScanner(f)
-	var mu sync.Mutex
-	lineBuffer := make([]string, 0, linesPerChunk)
+	results := make(chan StationMeasurements, 100000)
+	eg, _ := errgroup.WithContext(context.Background())
 
 	for i := 0; i < numWorkers; i++ {
-		eg.Go(func() error {
-			for {
-				select {
-				case <-ctx.Done():
-					return ctx.Err()
-				default:
-					mu.Lock()
-					if len(lineBuffer) == 0 {
-						for j := 0; j < linesPerChunk && scanner.Scan(); j++ {
-							lineBuffer = append(lineBuffer, scanner.Text())
-						}
-						if len(lineBuffer) == 0 {
-							mu.Unlock()
-							return nil // No more lines to process
-						}
-					}
-					chunk := make([]string, len(lineBuffer))
-					copy(chunk, lineBuffer)
-					lineBuffer = lineBuffer[:0]
-					mu.Unlock()
+		start := int64(i) * linesPerChunk
+		end := start + linesPerChunk
+		if i == numWorkers-1 {
+			end = info.Size()
+		}
 
-					localStations, err := processLines(chunk)
+		eg.Go(func() error {
+			lineBuffer := make([][]byte, 0, linesPerChunk/10)
+			// Create a section reader for this chunk
+			sectionReader := io.NewSectionReader(f, start, end-start)
+			reader := bufio.NewReader(sectionReader)
+			// If not the first worker, find the start of the next complete line
+			if start > 0 {
+				if err := skipUntilNewline(reader); err != nil {
+					return err
+				}
+			}
+
+			// Read lines until we reach the end of our chunk
+			for {
+				line, err := reader.ReadBytes('\n')
+				if err != nil {
+					if err == io.EOF {
+						// If we're not the last worker and we have a partial line
+						if len(line) > 0 && end < info.Size() {
+							// Create a new section reader that extends beyond our chunk
+							extendedReader := io.NewSectionReader(f, end, 1024) // Read up to 1KB more
+							extraReader := bufio.NewReader(extendedReader)
+
+							// Read until we find a newline
+							extraBytes, err := extraReader.ReadBytes('\n')
+							if err != nil && err != io.EOF {
+								return err
+							}
+
+							// Combine the partial line with the extra bytes
+							lineBuffer = append(lineBuffer, slices.Concat(line, extraBytes[:len(extraBytes)-1]))
+							// Process the final partial line before returning
+							localStations, err := processLines(lineBuffer)
+							if err != nil {
+								return err
+							}
+							results <- localStations
+						}
+						return nil
+					}
+					return err
+				}
+
+				lineBuffer = append(lineBuffer, line[:len(line)-1])
+				if int64(len(lineBuffer)) == linesPerChunk {
+					localStations, err := processLines(lineBuffer)
 					if err != nil {
 						return err
 					}
 					results <- localStations
+					lineBuffer = lineBuffer[:0]
 				}
 			}
 		})
 	}
 
-	go func() {
-		eg.Wait()
-		close(results)
-	}()
-
 	finalStations := make(StationMeasurements)
-	for localStations := range results {
-		for name, measurement := range localStations {
-			prevMeasurement := finalStations[name]
-			finalStations[name] = Measurement{
-				Min:   calc.Min(prevMeasurement.Min, measurement.Min),
-				Max:   calc.Max(prevMeasurement.Max, measurement.Max),
-				Mean:  calc.CumAverage(prevMeasurement.Index, prevMeasurement.Mean, measurement.Mean),
-				Index: prevMeasurement.Index + measurement.Index,
+	w := sync.WaitGroup{}
+	w.Add(1)
+	go func() {
+		defer w.Done()
+		for localStations := range results {
+			for name, measurement := range localStations {
+				prevMeasurement := finalStations[name]
+				finalStations[name] = Measurement{
+					Min:   calc.Min(prevMeasurement.Index, prevMeasurement.Min, measurement.Min),
+					Max:   calc.Max(prevMeasurement.Index, prevMeasurement.Max, measurement.Max),
+					Mean:  calc.CumAverage(prevMeasurement.Index, prevMeasurement.Mean, measurement.Mean),
+					Index: prevMeasurement.Index + measurement.Index,
+				}
 			}
 		}
-	}
+	}()
 
 	if err := eg.Wait(); err != nil {
+		close(results)
 		log.Fatalf("Error processing file: %v", err)
 	}
+	close(results)
+
+	w.Wait()
 
 	keys := maps.Keys(finalStations)
 	slices.Sort(keys)
 	for _, key := range keys {
-		fmt.Printf("%s=%.2f/%.2f/%.2f \n", key, finalStations[key].Min, finalStations[key].Mean, finalStations[key].Max)
+		fmt.Printf("%s=%.2f/%.2f/%.2f, occurences=%d\n", key, finalStations[key].Min, finalStations[key].Mean, finalStations[key].Max, finalStations[key].Index)
 	}
+
 	fmt.Printf("Time taken: %s\n", time.Since(now))
 }
